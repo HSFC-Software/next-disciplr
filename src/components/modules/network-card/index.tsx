2658362fd765bb4fe3591406fd5da194faca08c7
chard <calipay.richard@gmai>
2023-03-08 00:43:03 +0800

    network card

--- src/components/modules/network-card/index.tsx
+++ src/components/modules/network-card/index.tsx
@@ -11,10 +11,35 @@ type Props = {
 
 export default function NetworkCard(props: Props) {
   const { first_name, last_name, created_at, member_count, status } = props;
+  
 
   return (
-    <div className={styles.card}>
-      {first_name} {last_name} {created_at} {member_count} {status}
-    </div>
-  );
+      <>
+
+        {/* active */}
+        <div className={styles.card_main}>
+            <div className={styles.card_body}>
+                  <img className={styles.card_img} src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?crop=1:1" alt=""/>
+                  <div className={styles.card_status}>{member_count} {status}</div>
+                <div className={styles.card_content}>
+                    <div>{first_name} {last_name}</div>
+                    <div className={styles.card_createdAt}>{created_at}</div>
+                </div>
+            </div>    
+        </div>
+
+        {/* inactive */}
+          <div className={styles.card_main}>
+          <div className={styles.card_body_inactive}>
+                  <img className={styles.card_img} src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?crop=1:1" alt=""/>
+                  <div className={styles.card_status_inactive}>{member_count} {status}</div>
+                <div className={styles.card_content}>
+                    <div>{first_name} {last_name}</div>
+                    <div className={styles.card_createdAt}>{created_at}</div>
+                </div>
+            </div>    
+        </div> 
+
+        </>
+    )
 }
