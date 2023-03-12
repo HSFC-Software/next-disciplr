10ec95e531ac5a8b80923772e4de8710f0af3242
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2023-03-13 03:15:38 +0800

    style: avatar dynamic font-size

--- src/components/base/avatar/index.tsx
+++ src/components/base/avatar/index.tsx
@@ -4,12 +4,15 @@ type AvatarProps = {
   children?: ReactNode;
   imgSrc?: string;
   size: number;
+  fontSize: "xs" | "sm" | "base" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl";
 };
 
-export default function Avatar({ children, size }: AvatarProps) {
+export default function Avatar({ children, size, fontSize }: AvatarProps) {
   return (
     <div
-      className={`shrink-0 bg-[#eaeaea] h-[${size}px] w-[${size}px] rounded-full z-10 flex justify-center items-center font-bold text-xl uppercase`}
+      className={`shrink-0 bg-[#eaeaea] h-[${size}px] w-[${size}px] rounded-full z-10 flex justify-center items-center font-bold uppercase ${
+        fontSize ? fontSize : "text-xl"
+      }`}
     >
       {children}
     </div>
--- src/components/base/member-badge/badge.tsx
+++ src/components/base/member-badge/badge.tsx
@@ -1,3 +1,4 @@
+import Avatar from "@/components/base/avatar";
 import style from "./badge.module.scss";
 
 type Props = {
@@ -10,11 +11,15 @@ export default function MemberBadge(props: Props) {
   const { first_name, last_name, image_url } = props;
   return (
     <div className={style.badge_main}>
-      <img
+      {/* <img
         className={style.badge_img}
         src="https://assets.entrepreneur.com/content/3x2/2000/20200429211042-GettyImages-1164615296.jpeg?crop=1:1"
         alt=""
-      />
+      /> */}
+      <Avatar fontSize="base" size={40}>
+        {`${first_name?.charAt(0) ?? ""}${last_name?.charAt(0) ?? ""}`.trim()}
+      </Avatar>
+
       <div className={style.badge_name}>
         {first_name} {last_name}
       </div>
