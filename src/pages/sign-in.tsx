02ae332254066d34c6267efb5bb62a99d83416a6
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2023-03-13 01:14:36 +0800

    style: remove dark mode, change sign in content

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>HSFC Consolidation</title>
+        <title>Disciplr | Sign In</title>
         <meta name="description" content="Consolidation for Network Leader" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
@@ -20,7 +20,10 @@ export default function Home() {
       <main className="w-screen w-full h-screen flex items-center justify-center">
         <div className="text-center px-12">
           <h1 className="text-4xl mb-6 font-bold text-slate-700">
-            Let&apos;s make it simple and make it happen.
+            <div>Disciplr.</div>
+            <span className="font-light text-base">
+              Your monitor to member&apos;s spiritual growth
+            </span>
           </h1>
           <button
             onClick={handleClick}
