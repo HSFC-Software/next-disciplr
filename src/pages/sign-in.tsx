9581a0c7863522ce95e9189266abeab18216dab4
Sam Tagalog <98756288+samtags@users.noreply.github.com>
2023-03-23 03:51:51 +0800

    feat: apply favicon

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -15,7 +15,6 @@ export default function Home() {
         <title>Disciplr | Sign In</title>
         <meta name="description" content="Consolidation for Network Leader" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-screen w-full h-screen flex items-center justify-center">
         <div className="text-center px-12">
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -12,7 +12,6 @@ export function SignUp() {
         <title>Disciplr | Sign Up</title>
         <meta name="description" content="Consolidation for Network Leader" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="w-screen w-full h-screen flex items-center justify-center"></main>
     </>
