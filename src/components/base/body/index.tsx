f3699b295b55300a731fc34f3efa2c34f05dc3b5
Sam <svtagalog@joyride.com.ph>
2023-04-03 04:10:12 +0800

    feat: new navigation bar

--- src/components/base/body/index.tsx
+++ src/components/base/body/index.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function Body({ children }: { children?: ReactNode }) {
-  return <div className="grow overflow-y-auto">{children}</div>;
+  return <div className="grow">{children}</div>;
 }
--- src/components/templates/page/index.tsx
+++ src/components/templates/page/index.tsx
@@ -7,6 +7,7 @@ import { ModalProvider } from "@/components/base/modal/Provider";
 import { useToken } from "@/lib/hooks";
 import Link from "next/link";
 import { ReactNode, useEffect } from "react";
+import styles from "./index.module.scss";
 
 export default function Layout(props: {
   children?: ReactNode;
@@ -23,11 +24,11 @@ export default function Layout(props: {
       <Auth>
         <main className="w-screen h-screen flex flex-col bg-white relative text-gray-900">
           <div id="header-filler" />
-          <div id="content-area" className="grow flex-col flex overflow-hidden">
+          <div id="content-area" className="grow flex-col flex overflow-y-auto">
             {props?.header}
             {props.children}
+            <div id="footer-filler" />
           </div>
-          <div id="footer-filler" />
           <Nav activeRoute={activeRoute} />
         </main>
       </Auth>
@@ -43,9 +44,53 @@ function Nav(props: { activeRoute?: string }) {
     const nav = document.getElementById("nav");
 
     if (filler && nav) {
-      filler.style.marginBottom = `${nav.offsetHeight}px`;
+      filler.style.paddingBottom = `${nav.offsetHeight}px`;
     }
   });
+
+  return (
+    <nav
+      id="nav"
+      className="shrink-0 flex px-7 py-5 justify-center fixed bottom-7 w-screen z-10"
+    >
+      <div className={`${styles.navigation} px-14 py-5 bg-blue-200 bg-white`}>
+        <Link href="/networks">
+          <GroupIcon
+            width={38}
+            height={38}
+            isActive={activeRoute === "networks"}
+          />
+        </Link>
+        <Link href="/conso" className="relative flex items-center">
+          <DocumentIcon
+            width={38}
+            height={38}
+            isActive={activeRoute === "conso"}
+          />
+          <span className="text-[8px] absolute bg-[#FB5D64] text-white px-2 rounded-lg w-[50px] text-center ml-[-7px]">
+            Soon
+          </span>
+        </Link>
+        <Link href="/" className="relative flex items-center">
+          <NotificationIcon
+            width={38}
+            height={38}
+            isActive={activeRoute === "/"}
+          />
+          <span className="text-[8px] absolute bg-[#FB5D64] text-white px-2 rounded-lg w-[50px] text-center ml-[-7px]">
+            Soon
+          </span>
+        </Link>
+        <Link href="/profile">
+          <UserIcon
+            width={38}
+            height={38}
+            isActive={activeRoute === "profile"}
+          />
+        </Link>
+      </div>
+    </nav>
+  );
 
   return (
     <nav
@@ -70,7 +115,7 @@ function Nav(props: { activeRoute?: string }) {
             height={30}
             isActive={activeRoute === "conso"}
           />
-          <span className="text-[8px] absolute bg-red-500 text-white px-2 rounded-lg top-0 ml-8">
+          <span className="text-[8px] absolute bg-[#FB5D64] text-white px-2 rounded-lg top-0 ml-8">
             Soon
           </span>
           <span className="text-xs text-[#686777]">Consolidations</span>
@@ -84,7 +129,7 @@ function Nav(props: { activeRoute?: string }) {
             height={30}
             isActive={activeRoute === "/"}
           />
-          <span className="text-[8px] absolute bg-red-500 text-white px-2 rounded-lg top-0 ml-8">
+          <span className="text-[8px] absolute bg-[#FB5D64] text-white px-2 rounded-lg top-0 ml-8">
             Soon
           </span>
           <span className="text-xs text-[#686777]">Notifcations</span>
