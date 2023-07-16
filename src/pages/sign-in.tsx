104a31ccd9e7730b54303324846ab3d14b96f761
Sam <svtagalog@joyride.com.ph>
2023-07-16 15:23:27 +0800

    feat: change password

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,8 +1,24 @@
-export const getServerSideProps = async () => {
+import { GetServerSidePropsContext } from "next";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  let protocol = "http";
+
+  if (process.env.NODE_ENV === "production") {
+    protocol += "s";
+  }
+
+  const baseUrl = protocol + "://" + ctx.req.headers.host;
+  let redirect_uri = `${baseUrl}/auth`;
+
+  if (ctx.query.redirect_uri) {
+    redirect_uri += "?next=" + ctx.query.redirect_uri;
+  }
+
+  let destination = `https://sso.fishgen.org?client_id=disciplr&response_type=token&redirect_uri=${redirect_uri}&scope=openid%20profile%20email&state=disciplr`;
+
   return {
     redirect: {
-      // destination: `https://sso.fishgen.org?client_id=disciplr&response_type=token&redirect_uri=http://localhost:3001/auth&scope=openid%20profile%20email&state=disciplr`,
-      destination: `https://sso.fishgen.org?client_id=disciplr&response_type=token&redirect_uri=https://app.fishgen.org/auth&scope=openid%20profile%20email&state=disciplr`,
+      destination: destination,
       permanent: true,
     },
   };
